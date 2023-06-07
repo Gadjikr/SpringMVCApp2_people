b7d612ac2b1f054aca1c2061ffdde3776c924f0c
Gadjik <groman161@yandex.ru>
2023-06-08 00:32:52 +0300

    jdbcTemplate ver 0.21 -Batch update , BatchController, /test-batch-update -ID always same(todo)

--- src/main/java/ru/gadjik/springpeople/dao/PersonDAO.java
+++ src/main/java/ru/gadjik/springpeople/dao/PersonDAO.java
@@ -1,13 +1,14 @@
 package ru.gadjik.springpeople.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.gadjik.springpeople.models.Person;
 
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +20,7 @@ import java.util.List;
 public class PersonDAO {
 
     private final JdbcTemplate jdbcTemplate;
+
     @Autowired
     public PersonDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -26,19 +28,18 @@ public class PersonDAO {
 
 
     public List<Person> index() {
-        List<Person> people = new ArrayList<>();
 
         return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     public Person show(int id) {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?",new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
                 .stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
         jdbcTemplate.update("INSERT INTO Person VALUES (10,?,?,?)",
-                person.getName(), person.getAge(), person.getEmail() );
+                person.getName(), person.getAge(), person.getEmail());
     }
 
     public void update(int id, Person updatedPerson) {
@@ -49,4 +50,59 @@ public class PersonDAO {
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
     }
+
+
+    /////////////////////////////////// test Batch Update vs Simple Update
+    public void testMultipleUpdate() {
+        List<Person> people = create1000People();
+
+        long before = System.currentTimeMillis();
+
+        for (Person person : people) {
+            jdbcTemplate.update("INSERT INTO Person VALUES (?,?,?,?)",
+                    person.getId(), person.getName(), person.getAge(), person.getEmail());
+        }
+
+//        for (int i = 0; i < people.size(); i++) {
+//            jdbcTemplate.update("INSERT INTO Person VALUES (?,?,?,?)",
+//                    people.get(i).getId(), people.get(i).getName(), people.get(i).getAge(), people.get(i).getEmail());
+//        }
+
+        System.out.println("Time " + (System.currentTimeMillis() - before));
+    }
+
+    public void testBatchUpdate(){
+        List<Person> people = create1000People();
+
+        long before = System.currentTimeMillis();
+
+        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES (?,?,?,?)", new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                preparedStatement.setInt(1, people.get(i).getId());
+                preparedStatement.setString(2, people.get(i).getName());
+                preparedStatement.setInt(3, people.get(i).getAge());
+                preparedStatement.setString(4, people.get(i).getEmail());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return people.size();
+            }
+        });
+
+        System.out.println("Time " + (System.currentTimeMillis() - before));
+    }
+
+    private List<Person> create1000People() {
+        List<Person> people = new ArrayList<Person>();
+
+        for (int i = 0; i < 1000; i++) {
+            people.add(new Person(i, "Person" + i, 25, "mail" + i + "@gmail.con"));
+        }
+
+        return people;
+    }
+
+
 }
